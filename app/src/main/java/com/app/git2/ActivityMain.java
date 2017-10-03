3ef5f53ebc427fdebc979f5f671d633692eee495
gitTest <shayesteh@dehnad.co>
2017-10-03 12:16:00 +0330

    just some changes

--- app/src/main/java/com/app/git2/ActivityMain.java
+++ app/src/main/java/com/app/git2/ActivityMain.java
@@ -3,11 +3,15 @@ package com.app.git2;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class ActivityMain extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void test() {
+        int i = 0;
     }
 }
---
+++ app/src/main/java/com/app/git2/Activitysecond.java
@@ -0,0 +1,14 @@
+package com.app.git2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activitysecond extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+    }
+}
